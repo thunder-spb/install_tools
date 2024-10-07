80a035abf0ab40e0903b6c2c6ebd9b29aee78a50
Alexzander thunder Shevchenko <thunder@blackdeath.ru>
2024-10-07 17:30:32 +0400

    Chore

--- install.tool.helm.plugin.ssm.sh
+++ install.tool.helm.plugin.ssm.sh
@@ -7,5 +7,6 @@ HELMSSM_NAME="helm-ssm-${OS}"
 { [ "${ARCH}" = "arm64" ] && HELMSSM_NAME="${HELMSSM_NAME}-arm" || true; }
 HELM_PLUGIN_DIR="$(helm env | grep HELM_PLUGINS | cut -d\" -f2)/helm-ssm"
 mkdir -p ${HELM_PLUGIN_DIR}
+
 wget -qO- https://github.com/codacy/helm-ssm/releases/download/${TOOL_HELM_SSM_VERSION}/${HELMSSM_NAME}.tgz | tar xz -C ${HELM_PLUGIN_DIR} \
   && chmod +x ${HELM_PLUGIN_DIR}/helm-ssm
