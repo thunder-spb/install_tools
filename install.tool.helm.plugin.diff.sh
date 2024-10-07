963a6de82cdbc7fc4830c635c0a4d8982414ea6b
Alexzander thunder Shevchenko <thunder@blackdeath.ru>
2024-10-07 17:30:17 +0400

    Cleanup unnecessary code for htlm plugin diff

--- install.tool.helm.plugin.diff.sh
+++ install.tool.helm.plugin.diff.sh
@@ -7,7 +7,6 @@ source ${SCRIPT_PATH}/install.tools.versions.sh
 
 { [ "${OS}" = "darwin" ] && OS="macos" || true; }
 HELMSSM_NAME="helm-ssm-${OS}"
-{ [ "${ARCH}" = "arm64" ] && HELMSSM_NAME="${HELMSSM_NAME}-arm" || true; }
 HELM_PLUGIN_DIR="$(helm env | grep HELM_PLUGINS | cut -d\" -f2)/helm-diff"
 mkdir -p ${HELM_PLUGIN_DIR}
 
